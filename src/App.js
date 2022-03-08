998a30d7bb47ee40768761f74f8c72f82d0468dd
Johannes Halvorsen <57711624+6pBit@users.noreply.github.com>
2022-03-08 10:15:29 +0100

    server ferdig, ikke testet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
+  const [backendData, setBackendData] = useState({data: null})
+
+
+  useEffect((callBackendAPI) => {
+      callBackendAPI()
+        .then(res => setBackendData({data: res.express}))
+        .catch(error => console.log(error))
+  })
+
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/*test*/}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {backendData.data}
         </p>
         <a
           className="App-link"
