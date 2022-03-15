2a6fc8f6d189792d7b24e6279bc9eec600ad02b6
Johannes Halvorsen <57711624+6pBit@users.noreply.github.com>
2022-03-15 13:39:46 +0100

    router endret

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,7 @@
 const path = require(`path`)
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 5000
 
 const publicPath = path.join(__dirname, '..', 'public')
 
@@ -9,6 +9,9 @@ app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
+})
+app.get('/express_backend', (req, res) => { 
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' })
 })
 
 app.listen(port, () => {
