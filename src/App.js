6625ed11f09363c5c1a8a31925b309bd00b672bd
Johannes Halvorsen <57711624+6pBit@users.noreply.github.com>
2022-03-15 13:51:50 +0100

    fjerna boilerplate i app

--- src/App.js
+++ src/App.js
@@ -29,19 +29,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/*test*/}
+        
         <p>
-          Edit <code>src/App.js</code> and save to reoad. {backendData.data}
+          {backendData.data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
