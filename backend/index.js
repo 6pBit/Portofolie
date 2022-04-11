cab0c2fd19807a1d49248445e6c7f756ef1afb46
Johannes Halvorsen <57711624+6pBit@users.noreply.github.com>
2022-04-11 13:28:06 +0200

    henter ut en verdi fra mongodb

--- backend/index.js
+++ backend/index.js
@@ -37,11 +37,10 @@ app.get("/api", (req, res) => {
   let db_connect = dbo.getDb("sample_mflix");
   db_connect
     .collection("movies")
-
-    .find({})
+    .find({title: "Blacksmith Scene"})
     .toArray(function (err, result) {
       if (err) throw err;
-      console.log(res.json(result));
+      res.json({message: `${result[0].title}`});
 
     });
   //res.json({ message: "Hello from server!" });
@@ -50,7 +49,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 
-app.get('*', (req, res) => {
+app.get("/", (req, res) => {
 
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
